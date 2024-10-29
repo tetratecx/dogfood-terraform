1763d9bb13f91a14201778bc198a26db300ef835
Sergey Marunich <marunich.s@gmail.com>
2024-10-29 17:04:05 -0400

    add aws_k8s module

---
+++ aws/k8s/variables.tf
@@ -0,0 +1,45 @@
+variable "name_prefix" {
+  type        = string
+  description = "name prefix"
+}
+
+variable "cluster_name" {
+  type        = string
+  description = "cluster name"
+}
+
+variable "region" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "vpc_subnets" {
+  description = "vnet subnet ids"
+}
+
+variable "k8s_version" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "output_path" {
+  type = string
+}
+
+variable "tags" {
+  type = map(any)
+}
+
+variable "lb_controller_helm_chart_version" {
+  default = "1.7.1"
+}
+
+variable "lb_controller_settings" {
+  default = { "controllerConfig" = { "featureGates" = { "SubnetsClusterTagCheck" : "false" } } }
+}
